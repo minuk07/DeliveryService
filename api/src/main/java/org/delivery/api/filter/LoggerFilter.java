2ad6bb3baa7cdd0efd07d352d2c3ad68c2ee5ea6
minuk07 <lmw3164@naver.com>
2025-02-04 16:41:25 +0900

    Filter를 이용하여 실행 후 request, response header, body 내용 로그로 찍어주기

---
+++ api/src/main/java/org/delivery/api/filter/LoggerFilter.java
@@ -0,0 +1,74 @@
+package org.delivery.api.filter;
+
+import jakarta.servlet.*;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Component;
+import org.springframework.web.util.ContentCachingRequestWrapper;
+import org.springframework.web.util.ContentCachingResponseWrapper;
+
+import java.io.IOException;
+
+//Filter를 이용하여 실행 후 request, response header, body 내용 로그로 찍어주기
+
+@Slf4j
+@Component
+public class LoggerFilter implements Filter {
+    @Override
+    public void doFilter(ServletRequest servletRequest, ServletResponse servletResponse, FilterChain filterChain) throws IOException, ServletException {
+
+        var req = new ContentCachingRequestWrapper((HttpServletRequest) servletRequest); //형변환 -> 생성자 메소드로 객체 생성
+        var res = new ContentCachingResponseWrapper((HttpServletResponse) servletResponse);
+
+        //--------------------------실행전-----------------------------//
+        filterChain.doFilter(req, res);
+        //--------------------------실행후-----------------------------//
+
+        //request 정보
+        var headerNames = req.getHeaderNames();
+        var headerValues = new StringBuilder();
+
+        headerNames.asIterator().forEachRemaining(headerKey -> {
+            var headerValue = req.getHeader(headerKey);
+
+            //ex ) authorization-token : ??? , user-agent : ???
+            headerValues
+                    .append("[")
+                    .append(headerKey)
+                    .append(": ")
+                    .append(headerValue)
+                    .append("] ");
+        });
+
+        var requestBody = new String(req.getContentAsByteArray());
+        var uri = req.getRequestURI();
+        var method = req.getMethod();
+
+        log.info(">>>>> uri : {} , method : {} , header : {} , body : {}", uri, method, headerValues, requestBody);
+
+        //response 정보
+
+        var responseHeaderValues = new StringBuilder();
+
+        res.getHeaderNames().forEach(headerKey -> {
+            var headerValue = res.getHeader(headerKey);
+
+            responseHeaderValues
+                    .append("[")
+                    .append(headerKey)
+                    .append(": ")
+                    .append(headerValue)
+                    .append(" , ");
+        });
+
+        var responseBody = new String(res.getContentAsByteArray());
+
+        log.info("<<<<< uri : {} , method : {} , header : {} , body : {}", uri, method, responseHeaderValues, responseBody);
+
+        //response 내용을 읽었기 때문에 초기화 필요
+
+        res.copyBodyToResponse();
+
+    }
+}
